9e05d040c07e270c03497c8e8927ab30a524e983
Lee-Chansu <tutle482@gmail.com>
2024-04-19 17:51:23 +0900

    productAdd 페이지 css 수정 중

--- src/routes/productAddPage.js
+++ src/routes/productAddPage.js
@@ -1,66 +1,90 @@
 import React from "react";
 import "../css/productAdd.css";
 
+//컴포넌트
 import { Nav } from "../components/nav";
 import { Detail } from "../components/detail";
 
 export const ProductAdd = () => {
   return (
     <>
-    <Nav></Nav>
-    <Detail></Detail>
+      <Nav></Nav>
+      <Detail></Detail>
       <div className="product-add">
         <div className="div">
-          <div className="button-box">
-            <div className="edit-btn">
-              <div className="text-wrapper">등록</div>
+          <div className="checkBoxWrapper">
+            <div className="textWrapper">
+              <h2>카테고리 분류</h2>
             </div>
-            <div className="cancle-btn">
-              <div className="text-wrapper">취소</div>
-            </div>
-          </div>
-          <div className="product-detail">
-            <div className="input" />
-            <div className="text-wrapper">제품상세설명</div>
-          </div>
-          <div className="sub-image">
-            <div className="image-add">
-              <div className="overlap-group">
-                <div className="text-wrapper">+</div>
+            <div className="categorySelect">
+              <div className="checkBox">
+                <label for="categoryAccessary">악세서리</label>
+                <input
+                  className="category accessary"
+                  type="checkbox"
+                  name="category accessary"
+                  value="accessary"
+                />
+              </div>
+              <div className="checkBox">
+                <label for="category pants">하의</label>
+                <input className="category pants" type="checkbox" />
+              </div>
+              <div className="checkBox">
+                <label for="category top">상의</label>
+                <input
+                  className="category top"
+                  type="checkbox"
+                  name="top"
+                  value="top"
+                />
+              </div>
+              <div className="checkBox">
+                <label for="category outer">아우터</label>
+                <input
+                  className="category outer"
+                  type="checkbox"
+                  name="outer"
+                  value="outer"
+                />
               </div>
             </div>
-            <div className="overlap-wrapper">
-              <div className="overlap-group">
-                <div className="text-wrapper">+</div>
-              </div>
-            </div>
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group">
-                <div className="text-wrapper">+</div>
-              </div>
-            </div>
-            <div className="text-wrapper">서브이미지 등록</div>
           </div>
-          <div className="main-image">
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group">
-                <div className="text-wrapper">+</div>
-              </div>
-            </div>
-            <div className="text-wrapper">메인이미지 등록</div>
+          <div className="name">
+            <label for="name">제품명: </label>
+            <input className="input" name="name" type="text" />
           </div>
+          <div className="price">
+            <label for="price">가격: </label>
+            <input className="input" name="price" />
+          </div>
+
           <div className="created-at">
-            <div className="text-wrapper">등록날짜</div>
-            <div className="input" />
+            <label for="created-at">등록날짜: </label>
+            <input className="input" name="created-at" />
           </div>
+
+          <div className="main-image">
+            <div className="imageAddWrapper">
+              <label for="imageAdd">메인이미지 등록</label>
+              <input className="imageAdd" type="file" />
+              <input className="uploadImageName" type="text" readOnly value="uploadFile"/>
+            </div>
+          </div>
+
+          <div className="sub-image">
+            <div className="imageAddWrapper">
+              <label>서브이미지 등록</label>
+              <input className="addImage" />
+            </div>
+          </div>
+
           <div className="product-option">
             <div className="overlap">
               <div className="text-wrapper">stock</div>
             </div>
             <div className="overlap">
               <div className="text-wrapper">size</div>
-              <img className="line" alt="Line" src="line.svg" />
-              <img className="polygon" alt="Polygon" src="polygon.svg" />
             </div>
             <div className="div-wrapper">
               <div className="text-wrapper">color</div>
@@ -71,60 +95,18 @@ export const ProductAdd = () => {
             </div>
             <div className="text-wrapper">제품재고수량</div>
           </div>
-          <div className="price">
-            <div className="text-wrapper">가격</div>
-            <div className="input" />
+
+          <div className="product-detail">
+            <p className="text-wrapper">제품상세설명 : </p>
+            <input className="input" />
           </div>
-          <div className="name">
-            <div className="text-wrapper">제품명</div>
-            <div className="input" />
-          </div>
-          <div className="category-detail">
-            <div className="category">
-              <div className="check-box" />
-              <div className="element">디테일</div>
+          <div className="button-box">
+            <div className="edit-btn">
+              <div className="text-wrapper">등록</div>
             </div>
-            <div className="category">
-              <div className="check-box" />
-              <div className="element">디테일</div>
+            <div className="cancle-btn">
+              <div className="text-wrapper">취소</div>
             </div>
-            <div className="overlap">
-              <div className="category">
-                <div className="check-box" />
-                <div className="element">디테일</div>
-              </div>
-              <div className="category">
-                <div className="check-box" />
-                <div className="element">디테일</div>
-              </div>
-            </div>
-            <div className="text-wrapper">디테일 분류</div>
-          </div>
-          <div className="navbar">
-            <div className="category">
-              <div className="text-wrapper0">category</div>
-            </div>
-            <div className="category">
-              <div className="text-wrapper0">category</div>
-            </div>
-            <div className="category">
-              <div className="text-wrapper0">category</div>
-            </div>
-            <div className="category">
-              <div className="text-wrapper0">category</div>
-            </div>
-            <div className="category">
-              <div className="text-wrapper0">category</div>
-            </div>
-            <div className="check-box" />
-            <div className="text-wrapper1">악세서리</div>
-            <div className="check-box" />
-            <div className="text-wrapper2">하의</div>
-            <div className="check-box" />
-            <div className="text-wrapper3">상의</div>
-            <div className="check-box" />
-            <div className="text-wrapper4">아우터</div>
-            <div className="text-wrapper">카테고리 분류</div>
           </div>
         </div>
       </div>
--- src/routes/productList.js
+++ src/routes/productList.js
@@ -4,15 +4,18 @@ import "../css/productList.css";
 //컴포넌트
 import { Nav } from "../components/nav";
 import { Detail } from "../components/detail";
+import { Link } from "react-router-dom";
 
 export const ProductList = () => {
   return (
     <>
       <Nav></Nav>
       <Detail></Detail>
-      <div className=" product-list">
+      <div className=" productList">
         <div className=" div">
-          <div className=" product-wrap">
+          <div className=" productWrap">
+            <div className="addBtnBox"></div>
+            <Link to="/productList/detail">
             <div className=" product">
               <div className=" image">
                 <div className=" image-wrapper">사진</div>
@@ -26,149 +29,7 @@ export const ProductList = () => {
                 </div>
               </div>
             </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className="image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className="image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className="image">
-                <div className=" image-wrapper">사진</div>
-              </div>
-              <div className=" info-box">
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
-            <div className=" product">
-              <div className=" info-box">
-                <div className=" image">
-                  <div className=" image-wrapper">사진</div>
-                </div>
-                <div className=" product-name">
-                  <div className=" text-wrapper">상품이름</div>
-                </div>
-                <div className=" product-info">
-                  <div className=" text-wrapper">상품설명</div>
-                </div>
-              </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
