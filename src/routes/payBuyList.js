9ce849683f4b284ea26a585c2b926607b150d315
Yoong5g5 <g5g5_@naver.com>
2024-04-19 15:46:05 +0900

    jihoedit

--- src/routes/payBuyList.js
+++ src/routes/payBuyList.js
@@ -1,44 +1,64 @@
 import React from "react";
 import "../css/payBuyList.css";
+import { Link } from "react-router-dom";
 
 export const PayBuyList = () => {
+  
+
   return (
     <div class="pay-buy-list">
       <div class="div">
-        <div class="pay-item">
-          <div class="item-info">
-            <div class="overlap"><div class="text-wrapper">상품명</div></div>
-            <div class="overlap-group"><div class="text-wrapper">상품가격</div></div>
-            <div class="div-wrapper"><div class="text-wrapper">배송상태</div></div>
-            <div class="overlap-2"><div class="text-wrapper-2">결제일시</div></div>
-            <div class="image-box"></div>
-          </div>
-          <div class="overlap-3"><div class="text-wrapper-3">X</div></div>
-          <div class="overlap-5"><div class="text-wrapper-3">V</div></div>
-        </div>
-        <div class="pay-item-2">
-          <div class="item-info">
-            <div class="overlap"><div class="text-wrapper">상품명</div></div>
-            <div class="overlap-group"><div class="text-wrapper">상품가격</div></div>
-            <div class="div-wrapper"><div class="text-wrapper">배송상태</div></div>
-            <div class="overlap-2"><div class="text-wrapper-2">결제일시</div></div>
-            <div class="image-box"></div>
-          </div>
-          <div class="overlap-3"><div class="text-wrapper-3">X</div></div>
-          <div class="overlap-5"><div class="text-wrapper-3">V</div></div>
-        </div>
-        <div class="overlap-6">
-          <div class="price"></div>
-          <div class="price-2"></div>
-          <div class="price-3"></div>
-          <div class="text-wrapper-5">처음화면</div>
-          <div class="text-wrapper-6">이전화면</div>
-          <div class="text-wrapper-7">이전화면</div>
-        </div>
         <div class="text-wrapper-8">구매 내역</div>
         <div class="nav">
-          <div class="user"><div class="text-wrapper-9">유저정보</div></div>
-          <div class="logo"><div class="text-wrapper-10">logo</div></div>
+          <div class="logo">
+            <div class="text-wrapper-10">logo</div>
+          </div>
+          <div class="user">
+            <div class="text-wrapper-9">유저정보</div>
+          </div>
+        </div>
+        <div class="pay-item">
+          <input type="checkbox" class="overlap-5" name="products"></input>
+          <div class="item-info">
+            <div class="overlap-2">
+              <div class="text-wrapper-2">결제일시 : db</div>
+            </div>
+            <div class="overlap">
+              <div class="text-wrapper">상품명 : db</div>
+            </div>
+            <div class="overlap-group">
+              <div class="text-wrapper">상품가격 : db</div>
+            </div>
+            <div class="div-wrapper">
+              <div class="text-wrapper">배송상태 : db</div>
+            </div>
+            <div class="image-box"></div>
+          </div>
+          <button class="overlap-3">X</button>
+        </div>
+        <div class="pay-item-2">
+          <input type="checkbox" class="overlap-5" name="products"></input>
+          <div class="item-info">
+            <div class="overlap-2">
+              <span class="text-wrapper-2">결제일시 : db</span>
+            </div>
+            <div class="overlap">
+              <div class="text-wrapper">상품명 : db</div>
+            </div>
+            <div class="overlap-group">
+              <div class="text-wrapper">상품가격 : db</div>
+            </div>
+            <div class="div-wrapper">
+              <div class="text-wrapper">배송상태 : db</div>
+            </div>
+            <div class="image-box"></div>
+          </div>
+          <button class="overlap-3">X</button>
+        </div>
+        <div class="overlap-6">
+          <Link to="/" class="price">처음화면</Link>
+          <Link to="#" class="price-2">재구매하기</Link>
+          <Link to="#" class="price-3">이전화면</Link>
         </div>
       </div>
     </div>
