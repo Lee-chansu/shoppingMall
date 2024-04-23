becae64d72681fd50cf2d4463a495dc367929080
hotcake123 <tjgyrms123@gmail.com>
2024-04-23 10:41:53 +0900

    0423 merge

---
+++ src/routes/mainNotLogin.js
@@ -0,0 +1,119 @@
+import React from "react";
+import "../css/mainNotLogin.css";
+
+export const MainNotLogin = () => {
+  return (
+    <div className="mainNotlogin">
+      <div className="div">
+        <div className="nav">
+          <div className="logo">
+            <div className="textWrapper">logo</div>
+          </div>
+          <div className="user">
+            <a className="textWrapper2" href="example.com" rel="noopener noreferrer" target="_blank">
+              로그인
+            </a>
+          </div>
+        </div>
+        <div className="category">
+          <div className="divWrapper">
+            <div className="textWrapper3">category</div>
+          </div>
+          <div className="category2">
+            <div className="textWrapper3">category</div>
+          </div>
+          <div className="category3">
+            <div className="textWrapper3">category</div>
+          </div>
+          <div className="category4">
+            <div className="textWrapper3">category</div>
+          </div>
+          <div className="category5">
+            <div className="textWrapper3">category</div>
+          </div>
+        </div>
+        <div className="visual" />
+        <div className="wrap">
+          <div className="product">
+            <div className="image">
+              <div className="textWrapper4">사진</div>
+            </div>
+            <div className="infoBox">
+              <div className="productName">
+                <div className="textWrapper5">상품이름</div>
+              </div>
+              <div className="productInfo">
+                <div className="textWrapper6">상품설명</div>
+              </div>
+            </div>
+          </div>
+          <div className="product2">
+            <div className="image">
+              <div className="textWrapper4">사진</div>
+            </div>
+            <div className="infoBox">
+              <div className="productName">
+                <div className="textWrapper5">상품이름</div>
+              </div>
+              <div className="productInfo">
+                <div className="textWrapper6">상품설명</div>
+              </div>
+            </div>
+          </div>
+          <div className="product3">
+            <div className="image">
+              <div className="textWrapper4">사진</div>
+            </div>
+            <div className="infoBox">
+              <div className="productName">
+                <div className="textWrapper5">상품이름</div>
+              </div>
+              <div className="productInfo">
+                <div className="textWrapper6">상품설명</div>
+              </div>
+            </div>
+          </div>
+          <div className="product4">
+            <div className="image">
+              <div className="textWrapper4">사진</div>
+            </div>
+            <div className="infoBox">
+              <div className="productName">
+                <div className="textWrapper5">상품이름</div>
+              </div>
+              <div className="productInfo">
+                <div className="textWrapper6">상품설명</div>
+              </div>
+            </div>
+          </div>
+          <div className="product5">
+            <div className="image">
+              <div className="textWrapper4">사진</div>
+            </div>
+            <div className="infoBox">
+              <div className="productName">
+                <div className="textWrapper5">상품이름</div>
+              </div>
+              <div className="productInfo">
+                <div className="textWrapper6">상품설명</div>
+              </div>
+            </div>
+          </div>
+          <div className="product6">
+            <div className="image">
+              <div className="textWrapper4">사진</div>
+            </div>
+            <div className="infoBox">
+              <div className="productName">
+                <div className="textWrapper5">상품이름</div>
+              </div>
+              <div className="productInfo">
+                <div className="textWrapper6">상품설명</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
