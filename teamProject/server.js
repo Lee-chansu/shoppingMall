e508fba30767c4d752a82ec9a0e3b2b547e3df80
smileman2008@naver.com <tutle482@gmail.com>
2024-04-23 17:28:21 +0900

    node에 module 설치

--- teamProject/server.js
+++ teamProject/server.js
@@ -14,11 +14,13 @@ app.use(express.json()); // json 형태로 데이터 처리
 app.use(express.urlencoded({ extended: true })); // queryString 형식의 데이터 처리
 
 // 3. listen - 포트번호 지정
-app.listen(8080, () => {
-  console.log("접속 성공! - http://localhost8080 ");
+app.listen(3000, () => {
+  console.log("접속 성공! - http://localhost3000 ");
 });
 
 // 4. 하위페이지들 - 라우팅
 app.get("/", (req, res) => {
   res.send("메인 접속성공!");
 });
+
+
