03d9176419cf6d67f5619c68a90ad44e0c966587
smileman2008@naver.com <tutle482@gmail.com>
2024-04-23 16:45:59 +0900

    productAddPage css 수정중

--- src/components/product.js
+++ src/components/product.js
@@ -5,11 +5,11 @@ export const Product = () => {
         <img className="productImg" src="" alt="제품 사진" />
       </div>
       <div className="info-box">
-        <div className="product-name">
-          <p className="text-wrapper">상품이름</p>
+        <div className="productName">
+          <p className="text">상품이름</p>
         </div>
-        <div className="product-info">
-          <p className="text-wrapper">상품설명</p>
+        <div className="productInfo">
+          <p className="text">상품설명</p>
         </div>
       </div>
     </div>
