6c1dbfd5630868789cc3840cb6ad23d5c62f54c4
smileman2008@naver.com <tutle482@gmail.com>
2024-04-24 10:35:51 +0900

    충돌 병합 해결

--- teamProject/server.js
+++ teamProject/server.js
@@ -1,10 +1,7 @@
 // 1. 모듈 - require
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
 const cors = require('cors');
-=======
->>>>>>> master
 
 const mysql = require("mysql2");
 const dbConfig = require("./db");
@@ -12,34 +9,15 @@ const db = mysql.createConnection(dbConfig);
 
 // 2. use, set - 등록
 app.use(express.static(__dirname + "/public"));
-<<<<<<< HEAD
 app.use(cors())
-=======
->>>>>>> master
 
 app.use(express.json()); // json 형태로 데이터 처리
 app.use(express.urlencoded({ extended: true })); // queryString 형식의 데이터 처리
 
 // 3. listen - 포트번호 지정
-<<<<<<< HEAD
-app.listen(3000, () => {
-  console.log("접속 성공! - http://localhost3000 ");
-=======
 app.listen(5000, () => {
   console.log("접속 성공! - http://localhost5000 ");
->>>>>>> master
 });
 
 // 4. 하위페이지들 - 라우팅
-app.get("/", (req, res) => {
-<<<<<<< HEAD
-  
-});
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '/react-project/build/index.html'))
-})
-=======
-  res.send("메인 접속성공!");
-});
->>>>>>> master
