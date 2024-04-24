020e4f5ef34b6c95f71345b3b3ee87387bac89c7
arin <yurins0103@naver.com>
2024-04-24 14:05:21 +0900

    payment css 상세수정

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -9,42 +9,31 @@ export const Payment = () => {
         <div className="paymentInner">
           <div className="payBox">
             <div className="userInfoBox">
-              <div className="email">
-                <div className="textWrapper">이메일</div>
+              <div className="emailBox">
+                <div className="email">이메일 :</div>
               </div>
-              <div className="carryReq">
-                <div className="div">배송요청사항</div>
+              <div className="addressBox">
+                <div className="address">주소 :</div>
+              </div>
+              <div className="carryBox">
+                <div className="carryRequest">배송요청사항 :</div>
               </div>
               <div className="howPayBox">
                 <div className="title">
                   <div className="textWrapper2">결제방식</div>
                 </div>
-                <div className="checkBox">
-                  <div className="ex">ex 1</div>
-                  <div className="textWrapper3">ex 2</div>
-                  <div className="ex2">ex 3</div>
+                <div className="paymentBox">
+                  <div className="payCase1">네이버페이</div>
+                  <div className="payCase2">카카오페이</div>
+                  <div className="payCase3">신용카드</div>
                 </div>
-              </div>
-              <div className="address">
-                <div className="textWrapper4">주소</div>
               </div>
             </div>
             <div className="payList">
               <div className="payInfo">
-                <div className="divWrapper">
-                  <div className="textWrapper5">나의 주문</div>
+                <div className="myOrder">
+                  <div className="myOrder">나의 주문</div>
                 </div>
-                <div className="title2">
-                  <div className="textWrapper5">총 주문금액</div>
-                </div>
-                <div className="title3">
-                  <div className="textWrapper5">배송비</div>
-                </div>
-                <div className="title4">
-                  <div className="textWrapper5">총 합계</div>
-                </div>
-              </div>
-              <div className="productBox">
                 <div className="product">
                   <div className="info">
                     <div className="textWrapper6">제품설명</div>
@@ -60,6 +49,17 @@ export const Payment = () => {
                   <div className="image">
                     <div className="textWrapper7">이미지</div>
                   </div>
+                </div>
+              </div>
+              <div className="productBox">
+                <div className="title2">
+                  <div className="orderSumText">총 주문금액 :</div>
+                </div>
+                <div className="title3">
+                  <div className="orderSumText">배송비 :</div>
+                </div>
+                <div className="title4">
+                  <div className="orderSumText">총 합계 :</div>
                 </div>
               </div>
             </div>
