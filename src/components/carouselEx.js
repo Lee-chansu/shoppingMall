acc9bc3487ae194d4597b8dc952ac84804624bd8
smileman2008@naver.com <tutle482@gmail.com>
2024-05-16 11:40:59 +0900

    banner 이미지 추가 및 캐러셀 슬라이드 react-bootstrap으로 구현

---
+++ src/components/carouselEx.js
@@ -0,0 +1,36 @@
+import { Image } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+
+function CarouselFadeExample() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <Image
+          src="../img/banner1.png"
+          alt="배너1"
+          style={{ width: "100%", height: "100%" }}
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="../img/banner2.png"
+          alt="배너2"
+          style={{ width: "100%", height: "100%" }}
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="../img/banner3.png"
+          alt="배너3"
+          style={{ width: "100%", height: "100%" }}
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselFadeExample;
--- src/components/visual.js
+++ src/components/visual.js
@@ -1,19 +1,18 @@
-import { Carousel } from "bootstrap";
+import CarouselEx from "../components/carouselEx.js";
 
 export function Visual() {
-  const banners = ["../img/banner1.png", "../img/여성구두로퍼.jpg"];
   return (
-    // <Carousel ></Carousel>
-    <div className="visual">
-      <button className="left"> left </button>
-      {banners.map((el, index) => {
-        return (
-          <div className="wrap">
-            <img src={el} alt="배너" />
-          </div>
-        );
-      })}
-      <button className="right"> right</button>
-    </div>
+    <CarouselEx></CarouselEx>
+    // <div className="visual">
+    //   <button className="left"> left </button>
+    //   {banners.map((el, index) => {
+    //     return (
+    //       <div className="wrap">
+    //         <img src={el} alt="배너" />
+    //       </div>
+    //     );
+    //   })}
+    //   <button className="right"> right</button>
+    // </div>
   );
 }
