ae0b0c03d2f8ab05cc70f1cdeec0c189a5447c4d
arin <yurins0103@naver.com>
2024-05-17 17:55:10 +0900

    review css 수정중

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,17 +1,49 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../css/review.css";
+
+//컴포넌트
+import ButtonBox from "../components/ButtonBox";
 import CustomButton from "../components/CustomButton";
 
 export const Review = () => {
+  const navigate = useNavigate();
+  const [reviewColor, setReviewColor] = useState("");
+  const [reviewSize, setReviewSize] = useState("");
+  const colorList = ["밝아요", "화면과 같아요", "어두워요"];
+  const sizeList = ["작아요", "정사이즈예요", "커요"];
+  const [textarea, setTextarea] = useState(
+    "다른 분들에게 도움이 될 수 있는 리뷰를 300자 이내로 작성해 주세요"
+  );
+  const pageButton = ["사진 첨부하기", "리뷰 등록하기"];
 
-  const [reviewColor, setReviewColor] = useState('');
-  const [reviewSize, setReviewSize] = useState('');
-  const colorList = ['밝아요', '화면과 같아요', '어두워요'];
-  const sizeList = ['작아요', '정사이즈예요', '커요'];
+  const handleFocus = () => {
+    if (
+      textarea ===
+      "다른 분들에게 도움이 될 수 있는 리뷰를 300자 이내로 작성해 주세요"
+    )
+      setTextarea(""); //초기text만 삭제
+  };
+
+  const handleChange = (e) => {
+    setTextarea(e.target.value); //사용자 내용 입력시마다 state update
+  };
+
+  const handleLinkBackMove = () => {
+    navigate("/"); //사진등록하기 페이지 설정
+  };
+
+  const handleLinkReviewMove = () => {
+    navigate("/"); //리뷰등록하기 페이지 설정
+  };
 
   return (
+    
     <div className="review">
       <div className="reviewWrap">
+      <div className="reviewTitle">
+      리뷰 작성하기
+    </div>
         <div className="productBox">
           <img className="productImg"></img>
           <div className="nameOptionBox">
@@ -20,7 +52,7 @@ export const Review = () => {
           </div>
         </div>
 
-        <hr className="line" />
+        {/* <hr className="line" /> */}
 
         <div className="reviewCheckBox">
           <span className="title">구매하신 상품의 만족도를 체크해주세요</span>
@@ -29,34 +61,42 @@ export const Review = () => {
 
         <div className="reviewColorBox">
           <span className="title">색상은 어떤가요?</span>
-          <br/>
+          <br />
           {colorList.map((val, idx) => {
             return (
-              <CustomButton
-                key={idx}
-                buttonTitle={val}
-                className={reviewColor == idx ? 'selected' : 'non-selected'}
-                handleLinkMove={() => {
-                  setReviewColor(idx);
-                }}
-              />
+              <>
+                <div className="selectButton">
+                  <CustomButton
+                    key={idx}
+                    buttonTitle={val}
+                    className={reviewColor == idx ? "selected" : "non-selected"}
+                    handleLinkMove={() => {
+                      setReviewColor(idx);
+                    }}
+                  />
+                </div>
+              </>
             );
           })}
         </div>
 
         <div className="reviewSizeBox">
           <span className="title">사이즈는 어떤가요?</span>
-          <br/>
+          <br />
           {sizeList.map((val, idx) => {
             return (
-              <CustomButton
-                key={idx}
-                buttonTitle={val}
-                className={reviewSize == idx ? 'selected' : 'non-selected'}
-                handleLinkMove={() => {
-                  setReviewSize(idx);
-                }}
-              />
+              <>
+                <div className="selectButton">
+                  <CustomButton
+                    key={idx}
+                    buttonTitle={val}
+                    className={reviewSize == idx ? "selected" : "non-selected"}
+                    handleLinkMove={() => {
+                      setReviewSize(idx);
+                    }}
+                  />
+                </div>
+              </>
             );
           })}
         </div>
@@ -64,18 +104,27 @@ export const Review = () => {
         <div className="reviewDetailBox">
           <span className="title">자세한 리뷰를 작성해 주세요</span>
           <div className="overlap-2">
-            <textarea className="reviewDetail">
-              다른 분들에게 도움이 될 수 있는 리뷰를 작성해 주세요
-            </textarea>
+            <textarea
+              className="reviewDetail"
+              value={textarea}
+              onFocus={handleFocus}
+              onChange={handleChange}
+            />
           </div>
         </div>
 
-        <div className="overlap-3">
-          <div className="text-wrapper-9">사진 첨부하기</div>
-        </div>
-        <div className="overlap-group-2">
-          <div className="text-wrapper-9">리뷰 등록하기</div>
-        </div>
+        <ButtonBox>
+          <CustomButton
+            className="btn1"
+            buttonTitle="사진 등록하기"
+            handleLinkMove={handleLinkBackMove}
+          />
+          <CustomButton
+            className="btn1"
+            buttonTitle="리뷰 등록하기"
+            handleLinkMove={handleLinkReviewMove}
+          />
+        </ButtonBox>
       </div>
     </div>
   );
