3d0680b20dd9285684f5ab29a13643335901b79a
jungmyeon <zhzsks123@daum.net>
2024-05-30 15:39:38 +0900

    05.30 리뷰완

--- src/components/productReview.js
+++ src/components/productReview.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../css/productReview.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const ProductReview = (props) => {
   const { switchBtn, setSwitchBtn, handleSwitchBtn, id, item } = props;
-
+  const navigate = useNavigate()
   const [userList, setUserList] = useState([]);
   const [reviewList, setReviewList] = useState([]);
   const starPoint = [0, 1, 2, 3, 4];
@@ -20,7 +20,7 @@ export const ProductReview = (props) => {
       .then((response) => response.json())
       .then((data) => setReviewList(data));
   }, []);
-
+  console.log(reviewList)
   return (
     <div className="productInfoReview">
       <div className="productInfoWrapper">
@@ -52,7 +52,8 @@ export const ProductReview = (props) => {
                       <div className="userName">{user.userName} 님의 리뷰</div>
                       {id === user.id ? (
                         <div className="editBtnForm">
-                          <button className="reviewEditBtn">
+                          <button className="reviewEditBtn"
+                            onClick={()=>{navigate('/payBuyList')}}>
                             리뷰 수정하기
                           </button>
                         </div>
@@ -81,16 +82,30 @@ export const ProductReview = (props) => {
                         );
                       })}
                     </div>
-                    <span className="evaluation">별점 5.0 / 매우 만족해요</span>
+                    <span className="evaluation">별점 : {el.starPoint} / 
+                    {el.starPoint === 1 ? "매우 별로에요":
+                      el.starPoint === 2 ? "별로에요" :
+                        el.starPoint === 3 ? "보통이에요" :
+                          el.starPoint === 4 ? "만족해요" :
+                            el.starPoint === 5 ? "매우 만족해요" : ""
+                    }</span>
                     <div className="reviewCreatedAt">
                       {el.reviewDate.substring(0, 10)}
                     </div>
-                    <div className="productColor">{el.reviewColor}/</div>
-                    <div className="productSize">{el.reviewSize}</div>
+                    <div className="productColor">{
+                      el.reviewColor===1?"밝아요":
+                        el.reviewColor===2?"화면과 같아요":
+                          el.reviewColor===3?"어두워요" : ""
+                    } /</div>
+                    <div className="productSize">{
+                      el.reviewSize===1?"작아요":
+                        el.reviewSize===2?"정사이즈에요":
+                          el.reviewSize===3?"커요":""
+                    }</div>
                     <div className="productDetail">{el.content}</div>
                     <img
                       className="reviewImage"
-                      src="https://asset.m-gs.kr/prod/93220173/1/550"
+                      src={el.reviewImage}
                       alt="reviewImage"
                     />
                   </div>
